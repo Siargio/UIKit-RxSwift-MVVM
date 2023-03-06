f0cc2c68fd32cf3cda82b841d33f45208b0db9af
Siarheimasiuk <winst73@gmail.com>
2023-03-06 10:01:50 +0300

    Initial Commit

---
+++ UIKit+RxSwift+MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit+RxSwift+MVVM
+//
+//  Created by Sergio on 6.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
